53063936fec0b494e4b79afba4edbe4ae61a6e44
wwoi03 <105768584+wwoi03@users.noreply.github.com>
2023-06-27 19:56:51 +0700

    Add project files.

---
+++ CNPM_DoAn_WebQuanLyKhachSan/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using CNPM_DoAn_WebQuanLyKhachSan.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CNPM_DoAn_WebQuanLyKhachSan.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
