175900b1b006867ff13875f0486c44de816d554c
wwoi03 <105768584+wwoi03@users.noreply.github.com>
2023-07-22 16:54:55 +0700

    mới nhất

---
+++ CNPM_DoAn_WebQuanLyKhachSan/Controllers/BookRoomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CNPM_DoAn_WebQuanLyKhachSan.Controllers
+{
+    public class BookRoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
