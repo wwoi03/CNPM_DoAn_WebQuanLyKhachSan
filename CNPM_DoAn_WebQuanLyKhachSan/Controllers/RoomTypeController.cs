475e496a280ca3bf6ccfffb7dbd601dafb4173cd
wwoi03 <105768584+wwoi03@users.noreply.github.com>
2023-08-02 01:15:31 +0700

    Code mới nhất

--- CNPM_DoAn_WebQuanLyKhachSan/Controllers/RoomTypeController.cs
+++ CNPM_DoAn_WebQuanLyKhachSan/Controllers/RoomTypeController.cs
@@ -1,19 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CNPM_DoAn_WebQuanLyKhachSan.Models;
+using CNPM_DoAn_WebQuanLyKhachSan.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CNPM_DoAn_WebQuanLyKhachSan.Controllers
 {
     public class RoomTypeController : Controller
     {
+        DBHelper dBHelper;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public RoomTypeController(DatabaseContext context, IWebHostEnvironment hostEnvironment)
+        {
+            dBHelper = new DBHelper(context);
+            _hostEnvironment = hostEnvironment;
+        }
         public IActionResult Index()
         {
             ViewData["PapeTitle"] = "Loại phòng";
+            ViewBag.roomTypes = dBHelper.GetRoomType();
             return View();
         }
 
+        // M: Thêm loại phòng
         public IActionResult Create()
         {
             ViewData["PapeTitle"] = "Loại phòng";
             return Json("aaa");
+        }
+
+        [HttpPost]
+        public IActionResult Create(RoomTypeVM roomTypeVM)
+        {
+            IFormFile imageFile = roomTypeVM.ImageFile;
+
+            if (ModelState.IsValid)
+            {
+                // Kiểm tra file ảnh
+                if (imageFile != null && imageFile.Length > 0)
+                {
+                    // Đảm bảo đường dẫn thư mục image
+                    var imagePath = Path.Combine(_hostEnvironment.WebRootPath, "images");
+
+                    // Tạo tên tệp ảnh duy nhất bằng cách sử dụng Guid và đuôi tệp ảnh ban đầu
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+
+                    // Kết hợp đường dẫn đến thư mục image và tên tệp ảnh để có đường dẫn đầy đủ
+                    var filePath = Path.Combine(imagePath, fileName);
+
+                    // Lưu tệp ảnh vào thư mục image
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        imageFile.CopyTo(stream);
+                    }
+
+                    // Lưu đường dẫn tệp ảnh vào thuộc tính Image của sản phẩm
+                    roomTypeVM.Image = fileName;
+                }
+
+                RoomType roomType = new RoomType()
+                {
+                    RoomTypeId = roomTypeVM.RoomTypeId,
+                    Name = roomTypeVM.Name,
+                    BedNumber = roomTypeVM.BedNumber,
+                    Description = roomTypeVM.Description,
+                    Price = roomTypeVM.Price,
+                    Image = roomTypeVM.Image,
+                };
+
+                dBHelper.InsertRoomType(roomType);
+                return RedirectToAction("Index");
+            }
+
+            return Json(roomTypeVM);
+        }
+
+        // M: Xóa loại phòng
+        [HttpGet]
+        public IActionResult Delete(int roomTypeId)
+        {
+            RoomType roomType = dBHelper.GetRoomTypeById(roomTypeId);
+            return Json(roomType);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteById(int roomTypeId)
+        {
+            dBHelper.DeleteRoomType(roomTypeId);
+            return RedirectToAction("Index");
         }
     }
 }
--- CNPM_DoAn_WebQuanLyKhachSan/wwwroot/js/admin5p.js
+++ CNPM_DoAn_WebQuanLyKhachSan/wwwroot/js/admin5p.js
@@ -604,98 +604,148 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 /* --------------------------------- RoomType --------------------------------- */
-if (document.getElementById('roomType-insert') != null) {
+// Create RoomType
+var roomTypeCreate = document.getElementById('roomType-create')
+if (roomTypeCreate != null) {
 	// Lắng nghe sự kiện khi người dùng bấm chỉnh sửa một nhân viên
-	document.getElementById('roomType-insert').addEventListener('click', function (e) {
-	//var employeeId = $(this).data("employee-id");
-	// Gọi Action GetEmployee bằng AJAX
-	console.log("mo");
-	$.ajax({
-		type: "GET",
-		url: "/RoomType/Create", // Đường dẫn tới Action GetEmployee
-		//data: { id: employeeId }, // Truyền tham số id cho Action GetEmployee
-		success: function (data) {
-			// Hiển thị khung chỉnh sửa với dữ liệu của nhân viên
-			var employeeDetailsHtml = `
-                        <!-- Lưu đặt phòng -->
-						<div class="panel-save d-flex justify-content-between align-items-center">
-							<span>Thêm mới ${data}</span>
+	roomTypeCreate.addEventListener('click', function (e) {
+		//var employeeId = $(this).data("employee-id");
+		// Gọi Action GetEmployee bằng AJAX
+		console.log("mofff");
+		$.ajax({
+			type: "GET",
+			url: "/RoomType/Create", // Đường dẫn tới Action GetEmployee
+			//data: { id: employeeId }, // Truyền tham số id cho Action GetEmployee
+			success: function (data) {
+				// Hiển thị khung chỉnh sửa với dữ liệu của nhân viên
+				var employeeDetailsHtml =
+					`
+						<form method="post" action="RoomType/Create" enctype="multipart/form-data">
+							<!-- Lưu đặt phòng -->
+							<div class="panel-save d-flex justify-content-between align-items-center">
+								<span>Thêm mới</span>
 
-							<div class="">
-								<a asp-controller="" asp-action="">Lưu</a>
-							</div>
-						</div>
-
-						<!-- Form -->
-						<form class="panel-form">
-							<!-- Thông tin -->
-							<div class="panel-form-info">
-								<!-- Tên tài khoản -->
-								<div class="panel-form-item">
-									<h5 class="panel-form-title">Tên tài khoản</h5>
-									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								<div class="">
+									<input class="panel-save-submit" type="submit" value="Lưu"/>
 								</div>
+							</div>
 
-								<div class="setting-account">
-									<div class="setting-account-title">
-										<i class="fa-solid fa-gear"></i>
-										<span>Thiết lập quyền hạn</span>
+							<!-- Form -->
+							<div class="panel-form">
+								<!-- Thông tin -->
+								<div class="panel-form-info">
+									<!-- Tên tài khoản -->
+									<div class="panel-form-item">
+										<h5 class="panel-form-title">Tên loại phòng</h5>
+										<input class="panel-form-input" type="text" name="Name"/>
+										<span data-valmsg-for="${data}" class="text-danger"></span>
 									</div>
 
-									<div class="setting-account-power">
-										<div class="setting-account-list">
-											<div class="setting-account-item">
-												<input type="checkbox" id="" value="" />
-												<label for="">Thuê trả phòng</label>
-											</div>
+									<!-- Tên tài khoản -->
+									<div class="panel-form-item">
+										<h5 class="panel-form-title">Số giường</h5>
+										<input class="panel-form-input" type="number" name="BedNumber"/>
+									</div>
 
-											<div class="setting-account-item">
-												<input type="checkbox" id="" value="" />
-												<label for="">Quản lý loại phòng</label>
-											</div>
+									<!-- Tên tài khoản -->
+									<div class="panel-form-item">
+										<h5 class="panel-form-title">Giá</h5>
+										<input class="panel-form-input" type="number" name="Price"/>
+									</div>
 
-											<div class="setting-account-item">
-												<input type="checkbox" id="" value="" />
-												<label for="">Quản lý tài khoản phụ</label>
-											</div>
+									<!-- Tên tài khoản -->
+									<div class="panel-form-item">
+										<h5 class="panel-form-title">Mô tả</h5>
+										<input class="panel-form-input" type="text" name="Description"/>
+									</div>
 
-											<div class="setting-account-item">
-												<input type="checkbox" id="" value="" />
-												<label for="">Quản lý tài Menu</label>
-											</div>
-										</div>
-
-										<div class="setting-account-list">
-											<div class="setting-account-item">
-												<input type="checkbox" id="" value="" />
-												<label for="">Đặt phòng</label>
-											</div>
-
-											<div class="setting-account-item">
-												<input type="checkbox" id="" value="" />
-												<label for="">Xem lịch sử</label>
-											</div>
-
-											<div class="setting-account-item">
-												<input type="checkbox" id="" value="" />
-												<label for="">Quản lý menu</label>
-											</div>
-										</div>
+									<!-- Tên tài khoản -->
+									<div class="panel-form-item">
+										<h5 class="panel-form-title">Hình ảnh</h5>
+										<input class="panel-form-input" accept="image/*" type="file" name="ImageFile"/>
 									</div>
 								</div>
 							</div>
 						</form>
-                    `;
-			$(".right-panel").html(employeeDetailsHtml);
-		},
-		error: function () {
-			alert("Đã xảy ra lỗi khi lấy thông t");
-		}
+					`;
+				$(".right-panel").html(employeeDetailsHtml);
+			},
+			error: function () {
+				alert("Đã xảy ra lỗi khi lấy thông t");
+			}
+		});
 	});
-});
 }
 
+// Delete RoomType
+var roomTypeDelete = document.querySelectorAll('.roomType-delete')
+if (roomTypeDelete != null) {
+	// Lắng nghe sự kiện khi người dùng bấm chỉnh sửa một nhân viên
+	roomTypeDelete.forEach((ele, index) => {
+		ele.addEventListener('click', function (e) {
+			var employeeId = $(this).data('room-type-id');
 
+			// Gọi Action GetEmployee bằng AJAX
+			$.ajax({
+				type: "GET",
+				url: "/RoomType/Delete?roomTypeId=" + employeeId, // Đường dẫn tới Action GetEmployee
+				//data: { roomTypeId: employeeId }, // Truyền tham số id cho Action GetEmployee
+				success: function (data) {
+					console.log(data);
+					// Hiển thị khung chỉnh sửa với dữ liệu của nhân viên
+					var employeeDetailsHtml =
+						`
+						<form method="post" action="RoomType/DeleteById?roomTypeId=${data.roomTypeId}">
+							<!-- Lưu đặt phòng -->
+							<div class="panel-save d-flex justify-content-between align-items-center">
+								<span>Thêm mới</span>
+
+								<div class="">
+									<input type="submit" value="Xóa"/>
+								</div>
+							</div>
+
+							<!-- Form -->
+							<div class="panel-form">
+								<!-- Thông tin -->
+								<div class="panel-form-info">
+									<!-- Tên tài khoản -->
+									<div class="panel-form-item">
+										<h5 class="panel-form-title">Tên loại phòng</h5>
+										<input class="panel-form-input" type="text" value="${data.name}" readonly/>
+									</div>
+
+									<!-- Tên tài khoản -->
+									<div class="panel-form-item">
+										<h5 class="panel-form-title">Số giường</h5>
+										<input class="panel-form-input" type="text" value="${data.bedNumber}" readonly/>
+									</div>
+
+									<!-- Tên tài khoản -->
+									<div class="panel-form-item">
+										<h5 class="panel-form-title">Giá</h5>
+										<input class="panel-form-input" type="text" value="${data.price}" readonly/>
+									</div>
+
+									<!-- Tên tài khoản -->
+									<div class="panel-form-item">
+										<h5 class="panel-form-title">Mô tả</h5>
+										<input class="panel-form-input" type="text" value="${data.description}" readonly/>
+									</div>
+								</div>
+							</div>
+						</form>
+					`;
+					$(".right-panel").html(employeeDetailsHtml);
+				},
+				error: function () {
+					alert("Đã xảy ra lỗi khi lấy thông t");
+				}
+			});
+		});
+    })
+	
+}
 /* --------------------------------- Staff --------------------------------- */
 if (document.getElementById('account-edit') != null) {
 	// Lắng nghe sự kiện khi người dùng bấm chỉnh sửa một nhân viên
@@ -788,6 +838,8 @@ if (document.getElementById('account-edit') != null) {
 });
 }
 
+
+// Details staff
 var accountDetails = document.getElementById('account-details')
 if (accountDetails != null) {
 	// Lắng nghe sự kiện khi người dùng bấm chỉnh sửa một nhân viên
@@ -798,6 +850,148 @@ if (accountDetails != null) {
 		$.ajax({
 			type: "GET",
 			url: "/Staff/Details", // Đường dẫn tới Action GetEmployee
+			//data: { id: employeeId }, // Truyền tham số id cho Action GetEmployee
+			success: function (data) {
+				// Hiển thị khung chỉnh sửa với dữ liệu của nhân viên
+				var employeeDetailsHtml =
+					`
+                        <!-- Lưu đặt phòng -->
+						<div class="panel-save d-flex justify-content-between align-items-center">
+							<span>Thêm mới ${data}</span>
+
+							<div class="">
+								<a asp-controller="" asp-action="">Lưu</a>
+							</div>
+						</div>
+
+						<!-- Form -->
+						<form class="panel-form">
+							<!-- Thông tin -->
+							<div class="panel-form-info">
+								<!-- Tên tài khoản -->
+								<a class="panel-form-item" href="/Admin/Index?mS=${12}">
+									<h5 class="panel-form-title">Mã nhân viên</h5>
+									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								</a>
+
+								<!-- Tên tài khoản -->
+								<div class="panel-form-item">
+									<h5 class="panel-form-title">Tên tài khoản</h5>
+									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								</div>
+
+								<!-- Tên tài khoản -->
+								<a class="panel-form-item">
+									<h5 class="panel-form-title">Tên nhân viên</h5>
+									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								</a>
+
+								<!-- Tên tài khoản -->
+								<div class="panel-form-item">
+									<h5 class="panel-form-title">Chức vụ</h5>
+									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								</div>
+
+								<div class="setting-account">
+									<div class="setting-account-title">
+										<i class="fa-solid fa-gear"></i>
+										<span>Thiết lập quyền hạn</span>
+									</div>
+
+									<div class="setting-account-power">
+										<div class="setting-account-list">
+											<div class="setting-account-item">
+												<input type="checkbox" id="" value="" />
+												<label for="">Thuê trả phòng</label>
+											</div>
+
+											<div class="setting-account-item">
+												<input type="checkbox" id="" value="" />
+												<label for="">Quản lý loại phòng</label>
+											</div>
+
+											<div class="setting-account-item">
+												<input type="checkbox" id="" value="" />
+												<label for="">Quản lý tài khoản phụ</label>
+											</div>
+
+											<div class="setting-account-item">
+												<input type="checkbox" id="" value="" />
+												<label for="">Quản lý tài Menu</label>
+											</div>
+										</div>
+
+										<div class="setting-account-list">
+											<div class="setting-account-item">
+												<input type="checkbox" id="" value="" />
+												<label for="">Đặt phòng</label>
+											</div>
+
+											<div class="setting-account-item">
+												<input type="checkbox" id="" value="" />
+												<label for="">Xem lịch sử</label>
+											</div>
+
+											<div class="setting-account-item">
+												<input type="checkbox" id="" value="" />
+												<label for="">Quản lý menu</label>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<!-- Tên tài khoản -->
+								<div class="panel-form-item">
+									<h5 class="panel-form-title">Giới tính</h5>
+									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								</div>
+
+								<!-- Tên tài khoản -->
+								<div class="panel-form-item">
+									<h5 class="panel-form-title">Ngày sinh</h5>
+									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								</div>
+
+								<!-- Tên tài khoản -->
+								<div class="panel-form-item">
+									<h5 class="panel-form-title">Số điện thoại</h5>
+									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								</div>
+
+								<!-- Tên tài khoản -->
+								<div class="panel-form-item">
+									<h5 class="panel-form-title">Địa chỉ</h5>
+									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								</div>
+
+								<!-- Tên tài khoản -->
+								<div class="panel-form-item">
+									<h5 class="panel-form-title">Email</h5>
+									<input class="panel-form-input" type="text" value="tuandao8826" readonly/>
+								</div>
+							</div>
+						</form>
+                    `;
+				$(".right-panel").html(employeeDetailsHtml);
+			},
+			error: function () {
+				alert("Đã xảy ra lỗi khi lấy thông t");
+			}
+		});
+	});
+}
+
+// Create staff
+var accountCreate = document.getElementById('account-create')
+if (accountCreate != null) {
+	// Lắng nghe sự kiện khi người dùng bấm chỉnh sửa một nhân viên
+	accountCreate.addEventListener('click', function (e) {
+		//var employeeId = $(this).data("employee-id");
+		// Gọi Action GetEmployee bằng AJAX
+		console.log("mofff");
+		$.ajax({
+			type: "GET",
+			url: "/Staff/Create", // Đường dẫn tới Action GetEmployee
 			//data: { id: employeeId }, // Truyền tham số id cho Action GetEmployee
 			success: function (data) {
 				// Hiển thị khung chỉnh sửa với dữ liệu của nhân viên
